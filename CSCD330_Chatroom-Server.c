9c0695290fd7c0b0b08afe5cdc4ba752e7f58a4e
Anh Tran Duc <atran14@eagles.ewu.edu>
2017-11-12 19:00:40 -0800

    Implement client initialization & server loop

--- CSCD330_Chatroom-Server.c
+++ CSCD330_Chatroom-Server.c
@@ -1,12 +1,87 @@
+#include <netinet/in.h>
+#include <strings.h>
 #include <stdio.h>
+#include <sys/select.h>
+
+#define STANDARD_NAME_LENGTH 8
+#define PORT 8080
+#define ROOM_COUNT 2
+#define USERS_CAP_PER_ROOM 3
+
+#define LIMBO "limbo"
+#define ROOM_1_ID "Hobby"
+#define ROOM_2_ID "330"
 
 typedef struct {
     int clisd;
-    char name[20];
-    int chatRoomId;
-};
+    char name[STANDARD_NAME_LENGTH];
+    char chatRoomId[STANDARD_NAME_LENGTH];
+} Client;
+
+/*******************  Function prototypes begin here  ************************/
+
+void preServerStart_InitializeClients(Client *, int);
+
+void preServerStart_InitializeNoWaitInterval(struct timeval *interval);
+
+/*******************  Function prototypes end here  ************************/
 
 int main() {
-    printf("Hello, World!\n");
+
+    Client clients[USERS_CAP_PER_ROOM * ROOM_COUNT];
+    fd_set current_fd_list, original_fd_list;
+    struct timeval noWait_Interval;
+    int io_ready_count;
+    int server_sd, max_fd;
+    struct sockaddr_in servaddr;
+
+    preServerStart_InitializeNoWaitInterval(&noWait_Interval);
+    preServerStart_InitializeClients(clients, USERS_CAP_PER_ROOM * ROOM_COUNT);
+
+    server_sd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+
+    bind(server_sd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    listen(server_sd, 10);
+
+    printf("[INFO] Server online at port %d\n", PORT);
+
+    FD_ZERO(&original_fd_list);
+    FD_SET(server_sd, &original_fd_list);
+    max_fd = server_sd;
+
+
+    while (1) {
+        current_fd_list = original_fd_list;
+        io_ready_count = select(max_fd + 1, &current_fd_list, NULL, NULL, &noWait_Interval);
+
+        //todo Detecting incoming connection
+
+        //
+    }
+
     return 0;
+}
+
+/****************************    Function implementation begins here    *********************************************/
+
+void preServerStart_InitializeClients(Client *clientArray, int clientCount) {
+    int i;
+    for (i = 0; i < clientCount; i++) {
+        bzero(clientArray[i].name, STANDARD_NAME_LENGTH);
+        bzero(clientArray[i].chatRoomId, STANDARD_NAME_LENGTH);
+
+        strncpy(clientArray[i].chatRoomId, LIMBO, strlen(LIMBO));
+        clientArray[i].clisd = -1;
+    }
+}
+
+void preServerStart_InitializeNoWaitInterval(struct timeval *interval) {
+    interval->tv_usec = 0;
+    interval->tv_sec = 0;
 }
